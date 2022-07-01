ae6fcef239ddeb7281f30a86efb3bcde60bb5070
hetur <heturpepa@gmail.com>
2022-07-01 01:51:50 -0300

    revicion 8

--- 04-JS-III/homework/homework.js
+++ 04-JS-III/homework/homework.js
@@ -237,8 +237,23 @@ function breakStatement(numero) {
   //devolver: "Se interrumpió la ejecución"
   //Pista: usá el statement 'break'
   // Tu código:
+  var arrayNumeros = [];
+  var suma = numero; 
+  for(var i = 0; i < 10; i++){
+      suma = suma + 2;
+      if(suma === i){
+        break;
+      }else{
+        arrayNumeros.push(suma);
+      }
+  }
+  if(arrayNumeros.length < 10){
+    return "Se interrumpió la ejecución";
+  }else if (arrayNumeros.length === 10){
+    return arrayNumeros;
+  }
 }
-
+  
 
 function continueStatement(numero) {
   //Iterar en un bucle aumentando en 2 el numero recibido hasta un límite de 10 veces.
@@ -247,6 +262,17 @@ function continueStatement(numero) {
   //Cuando el número de iteraciones alcance el valor 5, no se suma en ese caso y se continua con la siguiente iteración
   //Pista: usá el statement 'continue'
   // Tu código:
+  var vuelto = [];
+  var aumento = numero;
+  for(var i = 0; i < 10; i++){
+      if(i === 5){
+        continue;
+      }else{
+          aumento = aumento + 2;
+          vuelto.push(aumento);
+      } 
+  }
+  return vuelto;
 }
 
 

8f073011e765cd7b779e74203d5c17918e543f81
hetur <heturpepa@gmail.com>
2022-07-06 11:32:12 -0300

    first example

--- 05-JS-IV/homework/homework.js
+++ 05-JS-IV/homework/homework.js
@@ -6,13 +6,24 @@ function crearGato (nombre, edad) {
   // Agrega un método (funcion) llamado "meow" que devuelva el string "Meow!"
   // Devuelve el objeto
   // Tu código:
+     var obj = {
+      nombre: nombre,
+      edad: edad, 
+      meow: function meow() {
+        return "Meow!";
+      }
+  }
+  return obj;
 }
+
 
 function agregarPropiedad (objeto, property) {
   // Agrega una propiedad al objeto (argumento "objeto") con el valor `null`
   // Devuelve el objeto
   // NOTA: El nombre de la propiedad no es "propiedad", el nombre es el valor del argumento llamado "property" (una cadena/string)
   // Tu código:
+objeto [property] = null;
+  return objeto;
 }
 
 function invocarMetodo (objeto, metodo) {

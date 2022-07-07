cb3e34a19e55580137ee20b02bef57f07ead27d4
hetur <heturpepa@gmail.com>
2022-07-07 12:03:49 -0300

    six excercise

--- 05-JS-IV/homework/homework.js
+++ 05-JS-IV/homework/homework.js
@@ -82,6 +82,9 @@ function tienePropiedad (objeto, propiedad) {
   // "propiedad" es un string
   // De lo contrario, devuelve "false"
   // Tu código:
+ 
+  return objeto.hasOwnProperty(propiedad);
+
 }
 
 function verificarPassword (usuario, password) {
@@ -89,6 +92,12 @@ function verificarPassword (usuario, password) {
   // Devuelve "true" si coinciden
   // De lo contrario, devuelve "false"
   // Tu código:
+
+  if(usuario["password"] === password){
+    return true;
+  }else{
+    return false;
+  }
 }
 
 function actualizarPassword (usuario, nuevaPassword) {
